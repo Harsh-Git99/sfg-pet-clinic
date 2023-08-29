a21b126462558243d6e6f23a31eb3d9cca7ae9a7
Harsh Pratap Singh <38403576+Harsh-Git99@users.noreply.github.com>
2023-08-29 18:07:21 +0530

    Added VetList and updated controller. Closes #15

--- pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,27 @@
 package com.springframework.sfgpetclinic.controllers;
 
+import com.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @GetMapping({"/vets","/vets/index"})
-    public String listVets(){
-        return "vets/index";
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
+    @GetMapping({"","/","/index"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
+        return "vet/index";
     }
 }
