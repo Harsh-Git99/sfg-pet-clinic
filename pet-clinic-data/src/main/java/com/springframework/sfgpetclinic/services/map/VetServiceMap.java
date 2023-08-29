abb75e65f1d6aec0951c601ce80c04068f5250cc
Harsh Pratap Singh <38403576+Harsh-Git99@users.noreply.github.com>
2023-08-29 17:37:54 +0530

    adding spring config. Closes #22

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -4,9 +4,11 @@ import com.springframework.sfgpetclinic.model.Owner;
 import com.springframework.sfgpetclinic.model.Vet;
 import com.springframework.sfgpetclinic.services.CrudService;
 import com.springframework.sfgpetclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
