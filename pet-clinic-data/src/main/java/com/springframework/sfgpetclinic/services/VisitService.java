567598a087fd4b1d75b477d25adb5014baab816f
Harsh Pratap Singh <38403576+Harsh-Git99@users.noreply.github.com>
2023-09-04 01:02:30 +0530

    created Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,8 @@
+package com.springframework.sfgpetclinic.services;
+
+import com.springframework.sfgpetclinic.model.Visit;
+import com.springframework.sfgpetclinic.services.CrudService;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
